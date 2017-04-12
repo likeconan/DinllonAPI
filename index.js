4bf24c15fe754233bca2d0c06fa8e6ff42d67bbb
Conan Lee <liyijia@shinetechchina.com>
2017-04-12 17:54:14 +0800

    Fix CORS problem

--- index.js
+++ index.js
@@ -7,16 +7,31 @@ var restify = require('restify'),
 
 var server = restify.createServer(config.server);
 
-restify
-    .CORS
-    .ALLOW_HEADERS
-    .push('x-access-token');
+restify.CORS.ALLOW_HEADERS.push('x-access-token');
 
 server.use(restify.CORS({
     origins: ['http://localhost:3000']
 }))
 
-server.use(restify.fullResponse());
+function unknownMethodHandler(req, res) {
+    if (req.method.toLowerCase() === 'options') {
+        console.log('received an options method request');
+        var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'x-access-token', 'Origin', 'X-Requested-With']; // added Origin & X-Requested-With
+
+        if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+
+        res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+        res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+        return res.send(204);
+    } else
+        return res.send(new restify.MethodNotAllowedError());
+}
+
+server.on('MethodNotAllowed', unknownMethodHandler);
+
+
 
 server.use(restify.queryParser());
 server.use(restify.bodyParser({
